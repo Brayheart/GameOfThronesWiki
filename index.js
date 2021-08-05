5fa48e43f3ed6aac192f1689f0291ac2bec21ac8
Tyler Bray <broncobray77@gmail.com>
2021-08-05 15:05:48 -0700

    buttons

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
+var container = document.querySelector('#container')
+
 fetch('https://www.anapioficeandfire.com/api')
   .then(response => response.json())
-  .then(data => console.log(data));
+  .then(data => {
+    console.log(data)
+    const propertyNames = Object.keys(data);
+    propertyNames.forEach(el => {
+      var button = document.createElement('button')
+      button.textContent = el
+      button.addEventListener('click', apiCall)
+      container.appendChild(button)
+    })
+
+  });
+
+var apiCall = function(event){
+  var text = event.target.textContent
+  fetch("https://www.anapioficeandfire.com/api/" + text)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data)
+    })
+}
+
+
+
+
+
+
+// fetch('https://www.anapioficeandfire.com/api/books')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+// fetch('https://www.anapioficeandfire.com/api/characters')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+// fetch('https://www.anapioficeandfire.com/api/houses')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
